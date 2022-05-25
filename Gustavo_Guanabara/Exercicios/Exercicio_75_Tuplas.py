bd63af12e18b60616c1ba69a13aebb5869174244
gierrejunior <gierremartins@gmail.com>
2022-05-25 17:02:49 -0300

    Exercicio

---
+++ Gustavo_Guanabara/Exercicios/Exercicio_75_Tuplas.py
@@ -0,0 +1,9 @@
+num = (
+    int(input('Digite um número: ')),
+    int(input('Digite outro número: ')),
+    int(input('Digite mais um número: ')),
+    int(input('Digite o último número: ')),
+    )
+print('Você digitou os valores ')
+a = num.count(9)
+print('O valor 9 apareceu {a} vezes')
